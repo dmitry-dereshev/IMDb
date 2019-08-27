8df776999aad46bfe1be127c4f63cc738c7465e2
Dmitry Dereshev <mr.dereshev@gmail.com>
2019-08-27 09:13:39 +0100

    Automated multi-file .tsv fixes

---
+++ 01_tsv_to_csv_clean_fix.py
@@ -0,0 +1,26 @@
+# Part of IMDb project by Dmitry Dereshev
+# https://github.com/dmitry-dereshev/IMDb
+
+# This script looks through a folder to pick up .tsv files, remove '\N', and
+# convert them to Unicode-compliant .csv files.
+
+import pandas as pd
+import os
+
+def tsv_to_csv_pandas(data_in, data_out):
+    # Collect file paths into a variable. Checks for subfolders as well.
+    tsv_files = [os.path.join(root, name)
+    for root, dirs, files in os.walk(data_in)
+    for name in files]
+    
+    for i in tsv_files:
+        data = pd.read_csv(i, sep='\t')
+        data.replace('\\N', '', inplace=True)
+        print(data.dtypes)
+        save_to = data_out+'\\'+os.path.basename(i)
+        data.to_csv(save_to, index=0)
+
+
+data_in = 'D:\Downloads\IMBD\.tsv files'
+data_out = 'D:\Downloads\IMBD\.tsv files\.csv files'
+tsv_to_csv_pandas(data_in, data_out)
