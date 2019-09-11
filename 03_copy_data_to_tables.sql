9adc3849f8a88c099150b6520e2db49a32447c3b
Dmitry Dereshev <mr.dereshev@gmail.com>
2019-09-11 10:39:08 +0100

    Changed copy commands to include 4 tables

--- 03_copy_data_to_tables.sql
+++ 03_copy_data_to_tables.sql
@@ -2,27 +2,18 @@
 Part of IMDb project by Dmitry Dereshev. 2019 MIT Licence.
 https://github.com/dmitry-dereshev/IMDb
 
-This SQL query populates the 7 tables created by 02_create_tables.sql with the
+This SQL query populates the 4 tables created by 02_create_tables.sql with the
 data cleaned up by 01_tsv_to_csv_clean_fix.py
 */
 
-COPY ACTOR_INFO
-FROM 'path\to\name.basics.csv' DELIMITER ',' CSV HEADER;
+COPY PEOPLE
+FROM 'path\to\.csv' DELIMITER ',' CSV HEADER;
 
-COPY TITLE_TYPES
-FROM 'path\to\title.akas.csv' DELIMITER ',' CSV HEADER;
+COPY MEDIA
+FROM 'D:\Downloads\IMBD\.csv files\media.csv' DELIMITER ',' CSV HEADER;
 
-COPY TITLE_INFO
-FROM 'path\to\title.basics.csv' DELIMITER ',' CSV HEADER;
+COPY MEDIA_REGIONS
+FROM 'D:\Downloads\IMBD\.csv files\title.akas.short.csv' DELIMITER ',' CSV HEADER;
 
-COPY CREW_INFO
-FROM 'path\to\title.crew.csv' DELIMITER ',' CSV HEADER;
-
-COPY SERIES_EPISODES
-FROM 'path\to\title.episode.csv' DELIMITER ',' CSV HEADER;
-
-COPY PEOPLE_JOBS_CHARACTERS
-FROM 'path\to\title.principals.csv' DELIMITER ',' CSV HEADER;
-
-COPY TITLE_RATINGS
-FROM 'path\to\title.ratings.csv' DELIMITER ',' CSV HEADER;
+COPY CREW
+FROM 'path\to\.csv' DELIMITER ',' CSV HEADER;
