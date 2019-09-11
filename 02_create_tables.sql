01efc36faf6ab3019c9551cb456fc8fde24d7074
Dmitry Dereshev <mr.dereshev@gmail.com>
2019-09-11 10:38:50 +0100

    Changed tables to the 4 desired ones

--- 02_create_tables.sql
+++ 02_create_tables.sql
@@ -2,13 +2,15 @@
 Part of IMDb project by Dmitry Dereshev. 2019 MIT Licence.
 https://github.com/dmitry-dereshev/IMDb
 
-This SQL query creates 3 tables:
+This SQL query creates 4 tables:
 1. People with unique info about people
 2. Media with unique info about media
 3. Crew, which connects people and media
+4. Media regions, which has information about tranlsations of media into other
+   languages
 */
 
-CREATE TABLE PEOPLE -- Unique person's information
+CREATE TABLE PEOPLE -- Unique person's information derived from name.basics 
 (PERSON_ID TEXT PRIMARY KEY, 
 FIRST_NAME TEXT,
 LAST_NAME TEXT,
@@ -18,12 +20,9 @@ DEATH_YEAR INTEGER) ;
 CREATE TABLE MEDIA -- Unique media (films, series, games) information
 (MEDIA_ID TEXT PRIMARY KEY,
 ORIGINAL_TITLE TEXT,
-LOCALIZED_TITLE TEXT,
+MEDIA_TYPE TEXT,
 YEAR_PRODUCED INTEGER,
-MEDIA_TYPE TEXT, 
-MEDIA_REGION TEXT,
-MEDIA_LANGUAGE TEXT,
-MEDIA_RUNTIME_MINUTES INTEGER,
+MEDIA_RUNTIME_MINUTES FLOAT,
 MEDIA_ADULT_STATUS BOOLEAN,
 AVERAGE_RATING FLOAT,
 NUMBER_OF_VOTES INTEGER) ;
@@ -32,3 +31,9 @@ CREATE TABLE CREW -- Junction table between people and media
 (MEDIA_ID TEXT,
 PERSON_ID TEXT,
 JOB TEXT) ;
+
+CREATE TABLE MEDIA_REGIONS -- Info about tranlsations of a given media
+(MEDIA_ID TEXT,
+LOCALIZED_TITLE TEXT,
+MEDIA_REGION TEXT,
+IS_ORIGINAL_TITLE BOOLEAN) ;
