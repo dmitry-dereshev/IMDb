47479502e16036e3ad36237099710d0b36592385
Dmitry Dereshev <mr.dereshev@gmail.com>
2019-09-13 09:29:41 +0100

    Added new table names

--- 03_populate_tables.sql
+++ 03_populate_tables.sql
@@ -5,18 +5,18 @@ https://github.com/dmitry-dereshev/IMDb
 This SQL query populates the 4 tables created by 02_create_tables.sql with the
 data cleaned up by 01_tsv_to_csv_clean_fix.py
 */
-/*
+
 COPY PEOPLE
 FROM 'path\to\csv' DELIMITER ',' CSV HEADER;
 
 COPY MEDIA
-FROM 'D:\Downloads\IMBD\.csv files\Intermediate Results\title.basics.short.csv' DELIMITER ',' CSV HEADER;
+FROM 'path\to\csv' DELIMITER ',' CSV HEADER;
 
 COPY CREW
-FROM 'D:\Downloads\IMBD\.csv files\crew.csv' DELIMITER ',' CSV HEADER;
-*/
+FROM 'path\to\csv' DELIMITER ',' CSV HEADER;
+
 COPY MEDIA_REGIONS
-FROM 'D:\Downloads\IMBD\.csv files\media_regions.csv' DELIMITER ',' CSV HEADER;
+FROM 'path\to\csv' DELIMITER ',' CSV HEADER;
 
 COPY RATINGS
-FROM 'D:\Downloads\IMBD\.csv files\ratings.csv' DELIMITER ',' CSV HEADER;
+FROM 'path\to\csv' DELIMITER ',' CSV HEADER;
